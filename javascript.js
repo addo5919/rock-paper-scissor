039f02e689c85e3b503e692352d578456e6f763d
addo <chokkadiadityarao@gmail.com>
2022-11-20 12:53:59 +0530

    modify singleRound(), add decideWinner(), add 2 div for result

--- javascript.js
+++ javascript.js
@@ -12,75 +12,84 @@ function getComputerChoice() {
 }
 
 function singleRound(e) {
+    if(divGameResult.innerText != "") {
+        divGameResult.innerText="";
+    }
     let playerSelection=this.innerText;
     let computerSelection=getComputerChoice();
         if(playerSelection=="rock") {
         if(computerSelection=="rock") {
-             div.innerText="Its a draw!";
+             divSingleRound.innerText="Its a draw!";
         }
         else if(computerSelection=="paper") {
-            div.innerText="You loose! paper beats rock";
+            divSingleRound.innerText="You loose! paper beats rock";
+            computerScore++;
         }
         else if(computerSelection=="scissors") {
-            div.innerText="You win! rock beats scissors";
+            divSingleRound.innerText="You win! rock beats scissors";
+            playerScore++;
         }
     }
     if(playerSelection=="paper") {
         if(computerSelection=="rock") {
-            div.innerText="You win! paper beats rock";
+            divSingleRound.innerText="You win! paper beats rock";
+            playerScore++;
         }
         else if(computerSelection=="paper") {
-            div.innerText="Its a draw!";
+            divSingleRound.innerText="Its a draw!";
         }
         else if(computerSelection=="scissors") {
-            div.innerText="You loose! scissors beats paper";
+            divSingleRound.innerText="You loose! scissors beats paper";
+            computerScore++;
         }
     }
     if(playerSelection=="scissors") {
         if(computerSelection=="rock") {
-            div.innerText="You loose! rock beats scissors";
+            divSingleRound.innerText="You loose! rock beats scissors";
+            computerScore++;
         }
         else if(computerSelection=="paper") {
-            div.innerText="You win! scissors beats paper";
+            divSingleRound.innerText="You win! scissors beats paper";
+            playerScore++;
         }
         else if(computerSelection=="scissors") {
-            div.innerText="Its a draw!";
+            divSingleRound.innerText="Its a draw!";
         }
+    }
+    round++;
+    divSingleRound.innerText+=` player:${playerScore} `+
+    `computer:${computerScore} `+
+    `rounds over:${round}`;
+    if(round==5) {
+        decideWinner();
     }
 }
 
-function game() {
-let playerScore=0,computerScore=0;
-for(let i=0;i<5;i++) {
- let playerChoice=prompt("Enter rock,paper or scissors")
- let computerChoice=getComputerChoice();
- let result=singleRound(playerChoice,computerChoice);
- console.log(`player: ${playerChoice}\ncomputer: ${computerChoice}
- \nresult: ${result}`);
- if(result.search("win")!=-1) {
-    playerScore++;
- }
- else if(result.search("loose")!=-1) {
-    computerScore++;
- }
- console.log(`playerScore: ${playerScore}\ncomputerScore: ${computerScore}`);
-}
- 
-     
-if(playerScore>computerScore) {
-    console.log("You win the game!");
-}
-else if(playerScore<computerScore) {
-    console.log("You lost the game!");
-}
-else {
-    console.log("Its a draw!");
-}
+function decideWinner() {
+    if(playerScore > computerScore) {
+        divGameResult.innerText="You won the game! "+
+        "Click on rock,paper or scissors to play again.";   
+    }
+    else if(playerScore < computerScore) {
+        divGameResult.innerText="You lost the game! "+
+        "Click on rock,paper or scissors to play again.";  
+    }
+    else {
+        divGameResult.innerText="The game has ended in a draw! "+
+        "Click on rock,paper or scissors to play again."; 
+    }
+    playerScore=0;
+    computerScore=0;
+    round=0;
 }
 
-//game();
+
+let playerScore=0;
+let computerScore=0;
+let round=0;
 let buttons=document.querySelectorAll("button");
-let div=document.querySelector("div");
+let divSingleRound=document.querySelector("#single-round-result");
+let divGameResult=document.querySelector("#game-result");
 buttons.forEach((button)=>{
     button.addEventListener("click",singleRound);
-});
+}); 
